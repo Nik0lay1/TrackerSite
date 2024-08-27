ec200fe2a37be4d1ad8e7b419d60a405389b046c
Nik0lay1 <ua-man@ukr.net>
2024-08-27 07:34:58 +0200

    27.08_3
    Додавання функціоналу вибору мови на сторінці

---
+++ lang/en.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'title' => 'Main Page',
+    'welcome' => 'Welcome to Our Website',
+    'subtitle' => 'Your gateway to amazing features',
+    'feature_1' => 'Feature 1',
+    'benefit_1' => 'Benefit of Feature 1',
+    'feature_2' => 'Feature 2',
+    'benefit_2' => 'Benefit of Feature 2',
+    'feature_3' => 'Feature 3',
+    'benefit_3' => 'Benefit of Feature 3',
+    'feature_4' => 'Feature 4',
+    'benefit_4' => 'Benefit of Feature 4',
+    'name_placeholder' => 'Name',
+    'email_placeholder' => 'Email',
+    'register_button' => 'Register',
+];
